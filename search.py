85bade4de3166b9913260c6d55c464bc050e32d0
nedwards3 <nedwards3@ucmerced.edu>
2020-02-29 03:18:45 -0800

    Data

--- search.py
+++ search.py
@@ -5,5 +5,6 @@ output = []
 f = open( 'data.txt', 'r' ) #open the file in read universal mode
 for line in f:
     cells = line.split( " " )
-    output.append( ( cells[ 0 ], cells[ 1 ] ) ) #since we want the first, second column
+    output.append( ( cells[ 0 ], cells[ 1 ],cells[2],cells[3],cells[4],cells[5] ) ) #since we want the first, second column
 print (output)
+
