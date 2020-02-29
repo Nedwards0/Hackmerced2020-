cf78373cbad3dc8fea86190fffdf6c42f87180a8
nedwards3 <nedwards3@ucmerced.edu>
2020-02-29 06:25:13 -0800

    Update CostSearch.py

--- CostSearch.py
+++ CostSearch.py
@@ -1,4 +1,47 @@
-import csv    
+import csv   
+
+class node:##node
+    path=[]
+    classes=[]
+    
+    output = []
+    f= open( 'data.txt', 'r' ) #open the file in read univ ersal mode
+    for line in f:
+        cells = line.split( " " )
+        output.append( ( cells[ 0 ], cells[ 1 ],cells[2],cells[3],cells[4],cells[5] ) ) #since we want the first, second column
+
+        Course=None
+
+    def expand(self):
+        parent = self
+        for course,unit,pre1,pre2,pre3,pre4 in parent.output:
+            if (pre1 or pre2 or pre3 or pre4 == parent.Course):
+                parent.classes.append(course)
+        for x in parent.classes:
+            
+            child=node()
+            child.path=parent.path
+            child.path.append(parent.Course)
+            child.classes=parent.classes
+            child.Course=x
+            frontier.append(child)
+            print(child.Course)
+            
+    
+
+            
+
+            
+
+        
+    
+    
+frontier=[]
+
+
+class cancer:
+   ## while(True):
+        node=frontier.pop
 
 output = []
 
@@ -7,4 +50,23 @@ for line in f:
     cells = line.split( " " )
     output.append( ( cells[ 0 ], cells[ 1 ],cells[2],cells[3],cells[4],cells[5] ) ) #since we want the first, second column
 
-output[1]
+
+c=node() 
+
+for course,unit,pre1,pre2,pre3,pre4 in output:
+    if pre1 == "0":
+        c.classes.append(course)
+
+c.Course="CSE20"
+frontier.append(c)
+while True:
+    frontier.pop().expand()
+    print("cancer")
+
+
+
+
+
+
+
+
