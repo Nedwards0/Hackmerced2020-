46041ea2b4d0d8199b8c0c942941c9f0b1078cc2
nedwards3 <nedwards3@ucmerced.edu>
2020-02-29 01:42:43 -0800

    12

--- Data.py
+++ Data.py
@@ -1,13 +1,11 @@
-cars = ["1", "2", "3"]
+import time
+from selenium import webdriver
 
-with open('message.txt', 'r') as document:
-    answer = {}
-    for line in document:
-        line = line.split()
-        if not line:  # empty line?
-            continue
-        answer[line] = line[1:]
-print(answer)
-
-
-
+driver = webdriver.Chrome('/path/to/chromedriver')  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
